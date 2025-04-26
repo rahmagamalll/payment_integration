c82d574a63b7c8d4f328705bfc149a14917f56aa
rahmagamal <gamalrahma186@gmail>
2025-04-27 01:05:10 +0300

    - paypal payment is done

--- lib/core/helper/paypal_keys.dart
+++ lib/core/helper/paypal_keys.dart
@@ -1,1 +1,6 @@
-
+class PaypalKeys {
+  static const String clientId =
+      'AXcfHOgDsi06qYf45DftkHaCdehkBQsrRgeK1CZDTopPboWTpJjDcnc2nZ3sXYHT8LMpdA_N1-5U8Htu';
+  static const String secret =
+      'EMk0JghUAZhycBOwKmwJh8XP4Ov0hYXdkObbwPYE6gYnkfZy4gwRh-qecazRwcdTbqzcMpxphZ4fRknN';
+}
